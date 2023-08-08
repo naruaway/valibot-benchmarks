49f14649aa9a4f2e5637a0411d1f31876afd0729
Naru <naru@naruaway.com>
2023-08-08 18:31:42 +0900

    Update config template

--- config.ts
+++ config.ts
@@ -1,11 +1,19 @@
 import type { BenchmarkConfig } from "./src/types";
 
 export default {
-  // List of libraries to run benchmarks for. It will search inside ./libs directory first before looking into node_modules.
-  // You can use `@` to specify variants of libraries like `valibot@with-my-some-change`
+  /*
+   * List of libraries to run benchmarks for. It will search inside ./libs directory first before looking into node_modules.
+   * You can use `@` to specify variants of libraries like `valibot@with-my-some-change`.
+   * For example, if you want to compare different version of valibot, you can configure like the following:
+   *   libs: ["valibot@before", "valibot@after"],
+   *   baseline: "valibot@before"
+   *   target: "valibot@after"
+   * Then you need to put locally built version of valibot into `./libs/valibot@before` and `./libs/valibot@after` respectively.
+   */
   libs: ["valibot", "zod"],
-  target: "valibot",
   baseline: "zod",
+  target: "valibot",
+
   durationMs: 100,
   iterationsCount: 100,
 
