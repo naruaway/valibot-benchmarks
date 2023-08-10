fa4360bbc1e6f6dbf482b6acd87efb74be706233
Naru <naru@naruaway.com>
2023-08-11 01:42:17 +0900

    add scripts

---
+++ scripts/download-github-repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+repo=$1
+ref=$2
+targetdir=$3
+
+repotmpdir=.githubrepotmp
+
+mkdir $repotmpdir
+
+cd $repotmpdir
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+   "https://api.github.com/repos/$repo/zipball/$ref" > repo.zip
+
+unzip repo.zip
+rm repo.zip
+mv * ../"$targetdir"
+cd ..
+rmdir $repotmpdir
