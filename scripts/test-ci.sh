4ec42cd0ed19c1b005056c3e128ea0f31837a221
Naru <naru@naruaway.com>
2023-08-11 13:10:26 +0900

    add test-ci script

---
+++ scripts/test-ci.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+set -euo pipefail
+
+./scripts/prepare-valibot.sh fabian-hiller/valibot "b352de9cb9535f39ea682219b789e546cf3094bc^"
+./scripts/prepare-valibot.sh fabian-hiller/valibot "b352de9cb9535f39ea682219b789e546cf3094bc"
