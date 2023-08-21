d50e1b8f82fa23445e71004a2263b020c29bb717
Naru <naru@naruaway.com>
2023-08-21 13:13:53 +0900

    fix duration ms

--- npm-scripts/tmp-save-full.ts
+++ npm-scripts/tmp-save-full.ts
@@ -67,7 +67,7 @@ const libs = config.libs.map((lib) =>
 const benchmarkConfig: BenchmarkConfig = {
   libs,
 
-  durationMs: 1000,
+  durationMs: 100,
   iterationsCount: 100,
 
   runners: ["nodejs", "bun"],
